f69ad076a89bd1c707435b0dd359bece40856ea7
Robinlp <Robinlp@users.noreply.github.com>
2019-04-30 17:36:19 +0200

    .

--- Part3/src/Encode.java
+++ Part3/src/Encode.java
@@ -13,61 +13,54 @@ import java.util.logging.Logger;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Robin
  */
 public class Encode {
- 
+
     public static void main(String[] args) {
         String nameOfOriginalFile = args[0];
         String nameOfCompressedFile = args[1];
         FileInputStream input = null;
         FileOutputStream output = null;
-        
+
         int entries[] = new int[256];
         for (int i = 0; i < entries.length; i++) {
             entries[i] = 0;
         }
-        
+
         try {
             input = new FileInputStream(new File(nameOfOriginalFile));
+            int i;
+            while ((i = input.read()) != -1) {
+                entries[i]++;
+            }
         } catch (FileNotFoundException ex) {
             System.out.println("inputStream file exception");
             ex.printStackTrace();
+        } catch (IOException ex) {
+            System.out.println("IOException");
+            ex.printStackTrace();
+        } finally {
+            try {
+                input.close();
+            } catch (IOException ex) {
+                System.out.println("can't close");
+            }
+
         }
 
-        try {
-            output = new FileOutputStream(new File(nameOfCompressedFile));
-        } catch (FileNotFoundException ex) {
-            System.out.println("outputStream file exception");
-            ex.printStackTrace();
-        }
-        
-        
-        try {
-            int i;
-            while((i=input.read()) != -1){
-                entries[i]++;
-            }
-        } catch (IOException ex) {
-            System.out.println("output file exception");
-            ex.printStackTrace();
-        }finally{
-                try {
-                    input.close();
-                    output.close();
-                } catch (IOException ex) {
-                    System.out.println("can't close");
-                }
-            
-            
-            
-        }
-        
-        //System.out.println(Arrays.toString(entries));
-        
+        System.out.println(Arrays.toString(entries));
     }
+
+    
+     public Element Hoffman(int[] C){
+         int n = C.length;
+         PQ Q = new PQHeap(n);
+         for (int i = 0; i < n-2; i++) { //måske <=   -----------------------------------kig her
+             
+         }
+     }
     
 }
