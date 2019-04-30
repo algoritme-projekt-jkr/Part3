82ca430e43b87b6501831bbf777a74fdea997f16
Robinlp <Robinlp@users.noreply.github.com>
2019-04-30 18:29:36 +0200

    .

--- Part3/src/Encode.java
+++ Part3/src/Encode.java
@@ -18,6 +18,7 @@ import java.util.Arrays;
 public class Encode {
 
     public static void main(String[] args) {
+        Encode encode = new Encode();
         String nameOfOriginalFile = args[0];
         String nameOfCompressedFile = args[1];
         FileInputStream input = null;
@@ -50,30 +51,39 @@ public class Encode {
         }
 
         System.out.println(Arrays.toString(entries));
+        Element test = encode.createHoffmanTree(entries);
+        System.out.println("test key" + test.getKey() + "    data " + ((Node)test.getData()).getFrequency());
+        
     }
 
-    
-     public Element createHoffmanTree(int[] c){
-         int n = c.length;
-         PQ q = new PQHeap(n);
-         for (int i = 0; i < c.length; i++) {
-             q.insert(new Element(i, c[i]));
-         }
-         for (int i = 0; i < n-2; i++) { //måske <=   -----------------------------------kig her
-             Node z = new  Node(c[i]);
-             Node x = new Node((int)q.extractMin().getData());
-             z.setLeft(x);
-             
-             Node y = new Node((int)q.extractMin().getData());
-             z.setLeft(y);
-             
-             z.setFrequency(x.getFrequency() + y.getFrequency());
-             
-             q.insert(new Element(i, z));
-             
-         }
-         
-         return q.extractMin();
-     }
-    
+    public Element createHoffmanTree(int[] c) {
+        int tal= 0;
+        for (int i = 0; i < c.length; i++) {
+            tal += c[i];
+        }
+        System.out.println("tal: " + tal);
+        int n = c.length;
+        PQ q = new PQHeap(n);
+        for (int i = 0; i < c.length; i++) {
+            q.insert(new Element(c[i], null));
+        }
+        for (int i = 0; i < n - 2; i++) { //måske <=   -----------------------------------kig her
+            Node z = new Node(c[i]); //the new node of the hoffman tree
+            Node x = new Node(q.extractMin().getKey()); //we create a node x by extractMin.getData(), from q, to get the frequency
+            z.setLeft(x); //we set the left node of z to be x
+
+            //here we do the same thing with y as we did with x
+            Node y = new Node(q.extractMin().getKey());
+            z.setLeft(y);
+
+            //we set the frequency of z to be the sum of x and y'z frequency
+            z.setFrequency(x.getFrequency() + y.getFrequency());
+
+            q.insert(new Element(i, z)); // we insert z back into the q
+
+        }
+
+        return q.extractMin();
+    }
+
 }
