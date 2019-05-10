77aa9d068463a81b7ee855905995243bfae9b3cd
Robinlp <Robinlp@users.noreply.github.com>
2019-05-10 16:39:40 +0200

    Update Encode.java

--- Part3/src/Encode.java
+++ Part3/src/Encode.java
@@ -13,24 +13,35 @@ import java.io.IOException;
 public class Encode {
 
     public static void main(String[] args) {
-        Encode encode = new Encode(); //instantiate the class as an object we can use
-        String nameOfOriginalFile = args[0]; //the name of the input file that we get from the commandline
-        String nameOfCompressedFile = args[1]; //the name of the output file that we get from the commandline
-        FileInputStream input = null; //we initialice the input steam as a null value because we need it later
-        String[] huffmanPathTable; // a string array that will hold the 0/1 paths through the tree to the leaf/node with a character
-        BitOutputStream bitOutputStream = null; // we instantiate to null because we need it later
+        //instantiate the class as an object we can use
+        Encode encode = new Encode(); 
+        //the name of the input file that we get from the commandline
+        String nameOfOriginalFile = args[0]; 
+        //the name of the output file that we get from the commandline
+        String nameOfCompressedFile = args[1]; 
+        //we initialice the input steam as a null value because we need it later
+        FileInputStream input = null; 
+        // a string array that will hold the 0/1 paths through the tree
+        // to the leaf/node with a character
+        String[] huffmanPathTable; 
+        // we instantiate to null because we need it later
+        BitOutputStream bitOutputStream = null; 
 
-        int entries[] = new int[256]; // we make an array for the frequencies of each byte (from 0 to 255)
+        // we make an array for the frequencies of each byte (from 0 to 255)
+        int entries[] = new int[256]; 
         for (int i = 0; i < entries.length; i++) {
             entries[i] = 0; //we make sure that all indexes has the value 0
         }
         
-        //we have a try catch block because the different steams can throw exceptions
+   //we have a try catch block because the different steams can throw exceptions
         try {
-            input = new FileInputStream(new File(nameOfOriginalFile)); // now we instantiate the input stream with the input file
-            while (input.available() > 0) { //while there is anything left to read from the input
+            // now we instantiate the input stream with the input file
+            input = new FileInputStream(new File(nameOfOriginalFile)); 
+            //while there is anything left to read from the input
+            while (input.available() > 0) { 
                 int i = input.read(); //we read a byte
-                entries[i]++; //we increment the value at the index that corresponts to the byte
+                //we increment the value at the index that corresponts to the byte
+                entries[i]++; 
             }
             //we catch different exceptions
         } catch (FileNotFoundException ex) {
@@ -39,7 +50,9 @@ public class Encode {
         } catch (IOException ex) {
             System.out.println("IOException");
             ex.printStackTrace();
-        } finally { //when it is done we close the input. finally is called automatically when the try is done.
+        //when it is done we close the input. 
+        //finally is called automatically when the try is done.
+        } finally { 
             try {
                 input.close();
             } catch (IOException ex) {
@@ -47,25 +60,35 @@ public class Encode {
             }
 
         }
-        input = null; // we set the input to null to be 100% sure that we don't use the file anymore. Also because we are going to need to load the file again later
+        // we set the input to null to be 100% sure that we don't use the file anymore.
+        // Also because we are going to need to load the file again later
+        input = null; 
 
-        Element huffmanTree = encode.createHoffmanTree(entries); //we create the huffman tree using our PQHeap from part 1. createHoffmanTree returns the root element where we can get the root node.
-        huffmanPathTable = encode.huffmanTable((Node) huffmanTree.getData()); //we create the huffman table. we get the root node by casting the getData() to a node.
+        // we create the huffman tree using our PQHeap from part 1. 
+    //createHoffmanTree returns the root element where we can get the root node.
+        Element huffmanTree = encode.createHoffmanTree(entries); 
+        //we create the huffman table. we get the root node by casting the getData() to a node.
+        huffmanPathTable = encode.huffmanTable((Node) huffmanTree.getData()); 
 
         try {
-            bitOutputStream = new BitOutputStream(new FileOutputStream(nameOfCompressedFile)); //we instaniate the BitOutputStream that we received as part of the assignment
+            //we instaniate the BitOutputStream that we received as part of the assignment
+            bitOutputStream = new BitOutputStream(new FileOutputStream(nameOfCompressedFile)); 
             for (int i = 0; i < entries.length; i++) {
-                bitOutputStream.writeInt(entries[i]); //we write each value from the frequency table as an int to the output file
+                //we write each value from the frequency table as an int to the output file
+                bitOutputStream.writeInt(entries[i]); 
             }
-
-            input = new FileInputStream(new File(nameOfOriginalFile)); //we read the input file again
-            while (input.available() > 0) { // while there is anything left to read from the input 
+            
+            //we read the input file again
+            input = new FileInputStream(new File(nameOfOriginalFile)); 
+            // while there is anything left to read from the input 
+            while (input.available() > 0) { 
                 int i = input.read(); //we read a byte
-                //we look in the huffmanPathTable at the index of the byte we just read
-                //for each char at that index, we write that char (either 1 or 0) to the output.
+            //we look in the huffmanPathTable at the index of the byte we just read
+            //for each char at that index, we write that char (either 1 or 0) to the output.
                 for (int j = 0; j < huffmanPathTable[i].length(); j++) {
                     char c = huffmanPathTable[i].charAt(j);
-                    int o = Integer.parseInt(String.valueOf(c)); //we parse the char as an int, by using the valueOf in String
+                    //we parse the char as an int, by using the valueOf in String
+                    int o = Integer.parseInt(String.valueOf(c)); 
                     bitOutputStream.writeBit(o); //we write the bit to the output
                 }
 
@@ -98,11 +121,14 @@ public class Encode {
         int n = c.length; //the length of c
         PQ q = new PQHeap(n); //we instantiate our queue/PQHeap with the length n
         for (int i = 0; i < c.length; i++) {
-            q.insert(new Element(c[i], new Node(i, c[i]))); //we insert elements with the frequency as key and a new node with the character and frequency
+            //we insert elements with the frequency as key
+            //and a new node with the character and frequency
+            q.insert(new Element(c[i], new Node(i, c[i]))); 
         }
         for (int i = 0; i <= n - 2; i++) {
             Node z = new Node(c[i]); //the new node of the hoffman tree
-            Node x = (Node) q.extractMin().getData(); //we create a node x by extractMin.getData(), from q
+            //we create a node x by extractMin.getData(), from q
+            Node x = (Node) q.extractMin().getData(); 
             z.setLeft(x); //we set the left node of z to be x
 
             //here we do the same thing with y as we did with x
@@ -111,8 +137,9 @@ public class Encode {
 
             //we set the frequency of z to be the sum of x and y'z frequency
             z.setFrequency(x.getFrequency() + y.getFrequency());
-
-            q.insert(new Element(z.getFrequency(), z)); // we insert z back into the q
+            
+            // we insert z back into the q
+            q.insert(new Element(z.getFrequency(), z)); 
         }
         return q.extractMin();//we return the root
     }
@@ -123,9 +150,12 @@ public class Encode {
      * @return an array of strings for the path to each character in the huffman tree
      */
     public String[] huffmanTable(Node root) {
-        String huffmanCodes[] = new String[256]; //a string array with an index for each byte
-        StringBuilder sb = new StringBuilder(); //a stringBuilder for making the paths
-        return huffmanWalk(root, huffmanCodes, sb); //return the call to the recursive tree walk
+        //a string array with an index for each byte
+        String huffmanCodes[] = new String[256]; 
+        //a stringBuilder for making the paths
+        StringBuilder sb = new StringBuilder(); 
+        //return the call to the recursive tree walk
+        return huffmanWalk(root, huffmanCodes, sb); 
     }
     
     /**
